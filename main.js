72722420cbd71d5c0492e352c83f46327f391651
btkdevkhdelli5 <amcjkbtk91@outlook.com>
2022-03-28 11:21:36 +0200

    make it better

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
+const header = document.querySelector('.header')
 const menuBurger = document.querySelector('.__menu__burger')
 const navbar = document.querySelector('.navbar')
 const nav = document.querySelector('.nav')
 const firstbtn = document.querySelector('.__first__btn')
 const btnhidden = document.querySelector('.btn__hidden')
 
-menuBurger.addEventListener('click', () => {
+mapboxgl.accessToken = 'pk.eyJ1IjoibWJidGtkZXZqaW05MSIsImEiOiJja3R0cXNxYmIwM2J0MnBtbWgzMHpnbzBpIn0.2aoySEqBh3XRrXp3d2mg4g';
+new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [2.333333, 48.866667],
+  zoom: 10
+});
+
+document.addEventListener('scroll', (e) => {
+  if(scrollY > 0) {
+    header.classList.add('active')
+  } else {
+    header.classList.remove('active')
+  }
+})
+
+menuBurger.addEventListener('click', (e) => {
   navbar.classList.add('active')
   nav.classList.add('active')
+  navbar.style.top = `${parseInt(scrollY)}px`
   document.body.style.overflow = 'hidden'
 })
 
